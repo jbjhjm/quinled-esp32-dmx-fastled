f0757d5db2bebb4004729d116a0b1f809763b136
JANNIK-NOTEBOOK\Jannik <jm@wolfinteractive.de>
2025-11-06 18:23:47 +0100

    rewrite FastLED to run as task

--- src/app_main.cpp
+++ src/app_main.cpp
@@ -22,7 +22,7 @@
 #define DMX_REFRESH_MS 20
 
 CRGB leds[NUM_LEDS];
-static const char *TAG = "DMX";  // The log tagline.
+static const char *TAG = "APP_MAIN";  // The log tagline.
 static uint8_t data[DMX_PACKET_SIZE] = {};  // Buffer to store DMX data
 
 dmx_packet_t packet;
@@ -31,10 +31,11 @@ int packet_count = 0;
 TickType_t last_update = xTaskGetTickCount();
 
 TaskHandle_t dmxTaskHandle = NULL;
+TaskHandle_t fastLedTaskHandle = NULL;
 
 void dmxTask(void *pvParameters) {
 	int core = xPortGetCoreID();
-	Serial.print("begin to wait for DMX input\n");
+	Serial.print("begin to wait for DMX input");
 
 	while (true) {
 		// vTaskDelay(1000);
@@ -46,7 +47,7 @@ void dmxTask(void *pvParameters) {
 			
 			if (!is_connected) {
 				// Log when we first connect
-				Serial.printf(TAG, "DMX is connected.\n");
+				ESP_LOGI(TAG, "DMX is connected.");
 				is_connected = true;
 			}
 			
@@ -54,13 +55,13 @@ void dmxTask(void *pvParameters) {
 			if (now - last_update >= pdMS_TO_TICKS(DMX_REFRESH_MS)) {
 				dmx_read(DMX_NUM_1, data, DMX_PACKET_SIZE);
 
-				Serial.printf(TAG, "Start code: %02x, Size: %i, Packets/second: %i", packet.sc, packet.size, packet_count); 
+				ESP_LOGI(TAG, "Start code: %02x, Size: %i, Packets/second: %i", packet.sc, packet.size, packet_count); 
 				ESP_LOG_BUFFER_HEX(TAG, data, 16);  // Log first 16 bytes last_update = now; packet_count = 0;
 			}
 				
 		} else if (is_connected) {
 			// DMX timed out after having been previously connected
-			ESP_LOGI(TAG, "DMX was disconnected.\n");
+			ESP_LOGI(TAG, "DMX was disconnected.");
 			break;
 		}
 	}
@@ -72,28 +73,31 @@ void wait_for_serial_connection() {
 	while(!Serial && timeout_end > millis()) {}  //wait until the connection to the PC is established
 }
 
-void fastledTask() {
-	Serial.print("to red\n");
-	leds[0] = CRGB::Red;
-	FastLED.show();
-	delay(1000);
-	Serial.print("to black\n");
-	leds[0] = CRGB::Black;
-	FastLED.show();
-	delay(1000);
+void fastledTask(void *pvParameters) {
+	const TickType_t xDelay = 1000;
+	for(;;) {
+		ESP_LOGI(TAG,"to red");
+		leds[0] = CRGB::Red;
+		FastLED.show();
+		vTaskDelay(xDelay);
+		
+		ESP_LOGI(TAG,"to black");
+		leds[0] = CRGB::Black;
+		FastLED.show();
+		vTaskDelay(xDelay);
+	}
 }
 
 void shutdown() {
-	ESP_LOGI(TAG, "Uninstalling the DMX driver.\n");
+	ESP_LOGI(TAG, "Uninstalling the DMX driver.");
 	dmx_driver_delete(DMX_NUM_1);
 }
 
 
 
 extern "C" void app_main() {
-	Serial.print("booting...\n");
 	wait_for_serial_connection(); // Optional, but seems to help Teensy out a lot.
-	Serial.print("serial connection established\n");
+	ESP_LOGI(TAG, "=== serial connection established ===");
 	
 	dmx_config_t config = DMX_CONFIG_DEFAULT;
 	dmx_personality_t personalities[] = {
@@ -102,11 +106,11 @@ extern "C" void app_main() {
 	const int personality_count = 1;
 	dmx_driver_install(DMX_NUM_1, &config, personalities, personality_count);
 	dmx_set_pin(DMX_NUM_1, TX_PIN, RX_PIN, EN_PIN);
-	Serial.print("DMX configurated\n");
+	ESP_LOGI(TAG, "DMX configurated");
 	
 	FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(UncorrectedColor);
 	FastLED.setBrightness(BRIGHTNESS);
-	Serial.print("FastLED configurated\n");
+	ESP_LOGI(TAG, "FastLED configurated");
 
 	xTaskCreatePinnedToCore( // xTaskCreate() would run on random core
 		dmxTask, 
@@ -117,10 +121,21 @@ extern "C" void app_main() {
 		&dmxTaskHandle,
 		1 // core number to run on (0|1)
 	);
-	Serial.print("created DMX task\n");
+	ESP_LOGI(TAG, "created DMX task");
 
-	while(true) {
-		fastledTask();
-	}
+	xTaskCreatePinnedToCore( // xTaskCreate() would run on random core
+		fastledTask, 
+		"FastLED",
+		2048, // stack size
+		NULL, // task parameters
+		1, // priority
+		&fastLedTaskHandle,
+		0 // core number to run on (0|1)
+	);
+	ESP_LOGI(TAG, "created FastLED task");
+
+	// while(true) {
+	// 	fastledTask();
+	// }
 
 }
